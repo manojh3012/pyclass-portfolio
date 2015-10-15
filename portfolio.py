8f73fb151dec928fd6ffefc5412e5cd391ffa4d5
manojh3012 <manojh3012@gmail.com>
2015-10-15 16:34:43 -0700

    portfolio: Add nice printed formatting with namedtuple

--- portfolio.py
+++ portfolio.py
@@ -1,10 +1,16 @@
 '''Get a list of trades where trade is a tuple'''
+import pprint
+from collections import namedtuple
+
+Trade = namedtuple('Trade', ['symbol' ,'shares', 'price'])
 
 trades = []
 with open('notes/stocks.txt') as f:
       for line in f:
             line = line.rstrip()
             symbol, shares, price = line.split(',')
-            trade = symbol, int(shares), float(price)
-            print trade
+            trade = Trade(symbol, int(shares), float(price))
             trades.append(trade)            
+
+
+pprint.pprint(trades)
