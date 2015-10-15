83526a3aa48e1540a76389e26dfe0d6e441de08f
manojh3012 <manojh3012@gmail.com>
2015-10-15 16:20:37 -0700

    portfolio: Fully parsed stocks.txt file

--- portfolio.py
+++ portfolio.py
@@ -1,11 +1,10 @@
 '''Get a list of trades where trade is a tuple'''
 
+trades = []
 with open('notes/stocks.txt') as f:
       for line in f:
-            pass
-#            print line
-
-
-line = line.rstrip()
-fields = line.split(',')
-print fields
+            line = line.rstrip()
+            symbol, shares, price = line.split(',')
+            trade = symbol, int(shares), float(price)
+            print trade
+            trades.append(trade)            
